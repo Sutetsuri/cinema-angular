568a2de401fea1520c7b67d95894caf3d5c5c259
joelmu <joel.muittari@gmail.com>
2018-10-15 16:11:33 +0300

    day filter added

--- src/app/list/movie-list/movie-list.component.ts
+++ src/app/list/movie-list/movie-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from './../movie';
 import { MovieService } from './../movie-list.service';
 import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NgxXml2jsonService } from 'ngx-xml2json';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -15,14 +16,40 @@ import { NgxXml2jsonService } from 'ngx-xml2json';
 
 export class MovieComponent implements OnInit {
   movies: Movie[]
-  selectedMovie: Movie
+  selectedTime
+  xml
+  timeForm: FormGroup
+  times
+  selectedOption: string
 
-  xml = 'https://www.finnkino.fi/xml/Schedule/?area=1018';
 
-  constructor (private http: HttpClient, private ngxXml2jsonService: NgxXml2jsonService, movieService: MovieService) { }
+  constructor (private http: HttpClient, private ngxXml2jsonService: NgxXml2jsonService, movieService: MovieService, private fb: FormBuilder) { }
 
 
   ngOnInit() {
+    this.timeForm = this.fb.group({
+      timeControl: [Date.now()]
+    });
+    this.getTimes()
+    this.getMovies(Date.now())
+  }
+
+  getTimes() {
+    this.xml = 'https://www.finnkino.fi/xml/ScheduleDates/';
+    const parser = new DOMParser();
+    this.http.get(this.xml, {
+        responseType: 'text'
+      }).subscribe(data => {
+
+        const xml = parser.parseFromString(data, 'text/xml');
+        const obj = this.ngxXml2jsonService.xmlToJson(xml);
+        this.times = obj.Dates.dateTime;
+        },
+        error => console.log('oops', error))
+  }
+
+  getMovies(date) {
+    this.xml = 'https://www.finnkino.fi/xml/Schedule/?area=1018&dt='+date;
     const parser = new DOMParser();
     this.http.get(this.xml, {
         responseType: 'text'
@@ -31,12 +58,18 @@ export class MovieComponent implements OnInit {
         const xml = parser.parseFromString(data, 'text/xml');
         const obj = this.ngxXml2jsonService.xmlToJson(xml);
         this.movies = obj.Schedule.Shows.Show;
-        console.log(obj.Schedule.Shows.Show);
+        console.log(this.movies);
         },
         error => console.log('oops', error))
   }
-
-  selectMovie(movie: Movie) {
-    this.selectedMovie = movie
-  }
+  onSubmit() {
+    console.log(this.timeForm.value.timeControl);
+    this.selectedTime = this.timeForm.value.timeControl;
+    let year = this.selectedTime.substring(0, 4);
+    let month = this.selectedTime.substring(5, 7);
+    let day = this.selectedTime.substring(8, 10);
+    let date = day+'.'+month+'.'+year;
+    console.log(date);
+    this.getMovies(date);
+   }
 }
