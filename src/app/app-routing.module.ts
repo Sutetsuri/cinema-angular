fa3b1e94b3d93b85451c5bf3489df0c0fad41735
joelmu <joel.muittari@gmail.com>
2018-10-17 18:05:39 +0300

    page-not-found added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ReservationListComponent } from './list/reservations/reservation-list/reservation-list.component';
-
+import { MovieComponent } from './list/movie-list/movie-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'reservations', component: ReservationListComponent }
+  { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: 'movies', component: MovieComponent },
+  { path: 'reservations', component: ReservationListComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
